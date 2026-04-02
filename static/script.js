9a7e7012ef7710832149d79fb8be9f7945aa2a52
whoami92-cyber <niko.iijolainen@outlook.com>
2026-04-02 23:09:39 +0300

    update

---
+++ static/script.js
@@ -0,0 +1,7 @@
+console.log("yeah javascript works");
+window.onload= function(){
+  const button document.querySelector('button');
+  button.addEventListener('click', function() {
+    document.body.style.backgroundColor='red';
+          });
+  };
