8b576f17aa66d5bda53b9f8864ff13bfcddd382a
whoami92-cyber <niko.iijolainen@outlook.com>
2026-04-03 22:55:58 +0300

    update

--- webtesti.py
+++ webtesti.py
@@ -1,8 +1,18 @@
 from flask import Flask,render_template
+import json
+import sqlite3
 
 app = Flask(__name__)
-@app.route('/')
 
+def memory():
+    contnrct=sqlite3.contect('documemt.db')
+    cursor=conect.cursor()
+    cursor.ececute("SELECT * FROM  memory")
+    data=cursor.fetchall()
+    conect.close()
+    return data
+
+@app.route('/')
 def home():
     return  render_template('index.html')
 if __name__ =='__main__':
