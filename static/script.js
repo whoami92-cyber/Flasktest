1a0ebbef298786009bf84d67e7e708905755c9a6
whoami92-cyber <niko.iijolainen@outlook.com>
2026-04-04 16:02:34 +0300

    update

--- static/script.js
+++ static/script.js
@@ -2,7 +2,11 @@ console.log("yeah javascript works");
 window.onload= function(){
   const button=document.querySelector('.save_button');
   function cheansColor(){
-  document.body.style.backgroundColor='red'}
+  document.body.style.backgroundColor='red';
+  setTimeout(function(){
+    document.body.style.backgroundColor='lightgreen'}, 3000);
+  };
   button.addEventListener('touchstart',cheansColor);
   button.addEventListener('click', cheansColor);
-          };
+};
+  
